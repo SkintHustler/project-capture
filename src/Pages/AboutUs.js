e0a53dd25435336554c8445542138bd0a3492829
Maaz Ahmad <ahmad.maaz2233@gmail.com>
2021-04-04 00:02:21 +0500

    4. FAQ Section

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboustSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import Faqsection from "../components/FaqSection";
 const AboutUs = () => {
   return (
     <>
       <AboustSection />
       <ServicesSection />
+      <Faqsection />
     </>
   );
 };
