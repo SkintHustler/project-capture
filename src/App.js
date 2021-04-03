7128210f2436cfc860cbffb4802dbe5c153b3445
Maaz Ahmad <ahmad.maaz2233@gmail.com>
2021-04-04 00:48:13 +0500

    5. Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+//Global Style
+import GlobalStyle from "../src/components/GlobalStyle";
 // import Pages
 import AboutUs from "./Pages/AboutUs";
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
