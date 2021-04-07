85abac0504f5c249e04306bd3ed8c4a94566fe25
Maaz Ahmad <ahmad.maaz2233@gmail.com>
2021-04-07 13:55:35 +0500

    9. React Router Implementation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,27 @@ import GlobalStyle from "../src/components/GlobalStyle";
 // import Pages
 import AboutUs from "./Pages/AboutUs";
 import Nav from "./components/Nav";
+import ContactUs from "./Pages/ConactUs";
+import OurWork from "./Pages/OurWork";
+//Router
+import { Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Nav />
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
