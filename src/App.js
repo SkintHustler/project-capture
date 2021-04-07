216875660bb8aae1d9ee1ebee0abdc2d60b844e8
Maaz Ahmad <ahmad.maaz2233@gmail.com>
2021-04-07 12:04:46 +0500

    8. Initial Nav Bar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from "react";
 import GlobalStyle from "../src/components/GlobalStyle";
 // import Pages
 import AboutUs from "./Pages/AboutUs";
+import Nav from "./components/Nav";
 function App() {
   return (
     <div className="App">
+      <Nav />
       <GlobalStyle />
       <AboutUs />
     </div>
