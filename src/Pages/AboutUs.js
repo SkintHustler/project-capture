befd69990313023a924088ad690dabfe7b0ea693
Maaz Ahmad <ahmad.maaz2233@gmail.com>
2021-04-13 01:36:46 +0500

    ANIMA

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -2,13 +2,21 @@ import React from "react";
 import AboustSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import Faqsection from "../components/FaqSection";
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
       <AboustSection />
       <ServicesSection />
       <Faqsection />
-    </>
+    </motion.div>
   );
 };
 
